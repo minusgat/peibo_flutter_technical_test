1a03a60882f08fc4e93deac3544e2abec356236d
Carlos Hernández <minusgat@gmail.com>
2025-05-09 07:28:57 -0600

    add mock generation for Dio and DioApiClient in mocks.dart

---
+++ test/test_utils/mocks.dart
@@ -0,0 +1,8 @@
+import 'package:dio/dio.dart';
+import 'package:mockito/annotations.dart';
+import 'package:peibo_flutter_technical_test/chore/network/http/dio_client/dio_client.dart';
+
+@GenerateMocks([Dio, DioApiClient])
+// not required for mocks
+// ignore: no-empty-block
+void main() {}
