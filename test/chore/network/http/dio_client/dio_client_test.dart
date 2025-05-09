2b80948dec244d0f8d924393eecbeda2b5ad3b35
Carlos Hernández <minusgat@gmail.com>
2025-05-09 07:28:57 -0600

    add tests for DioApiClient to handle GET request success and failure scenarios

--- test/chore/network/http/dio_client/dio_client_test.dart
+++ test/chore/network/http/dio_client/dio_client_test.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:peibo_flutter_technical_test/chore/network/http/api_client.dart';
 import 'package:peibo_flutter_technical_test/chore/network/http/dio_client/dio_client.dart';
 
 import '../../../../test_utils/mocks.mocks.dart';
@@ -15,4 +17,33 @@ void main() {
     // Act && Assert
     expect(() => apiClient.get(''), throwsException);
   });
+
+
+  test(
+    'DioApiClient should return ApiResponse when GET request succeeds',
+    () async {
+      // Arrange
+      final mockApiClient = MockDio();
+      final apiClient = DioApiClient(dio: mockApiClient);
+
+      final dioResponse = Response(
+        data: {'key': 'value'},
+        requestOptions: RequestOptions(path: ''),
+      );
+
+      final expectedResponse = ApiResponse(
+        data: {'key': 'value'},
+        statusCode: 200,
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      when(mockApiClient.get('')).thenAnswer((_) async => dioResponse);
+
+      // Act
+      final response = await apiClient.get('');
+
+      // Assert
+      expect(response.data, expectedResponse.data);
+    },
+  );
 }
