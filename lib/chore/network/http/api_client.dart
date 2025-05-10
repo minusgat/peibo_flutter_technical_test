8e963d7b22fb37de8f04701bc87128b6c419d7e0
minusgat <40323741+minusgat@users.noreply.github.com>
2025-05-10 11:50:41 -0600

    ref(api-client): update ApiClient to return Result<ApiResponse<T>> (#9)

--- lib/chore/network/http/api_client.dart
+++ lib/chore/network/http/api_client.dart
@@ -1,20 +1,18 @@
+import 'package:peibo_flutter_technical_test/chore/utils/result.dart';
+
 class ApiResponse<T> {
   final T data;
   final int statusCode;
   final Map<String, String>? headers;
 
-  ApiResponse({
-    required this.data,
-    required this.statusCode,
-    this.headers,
-  });
+  ApiResponse({required this.data, required this.statusCode, this.headers});
 
   bool get isSuccessful => statusCode == 200;
   bool get hasFailed => statusCode != 200;
 }
 
 abstract class ApiClient {
-  Future<ApiResponse<T>> get<T>(
+  Future<Result<ApiResponse<T>>> get<T>(
     String path, {
     Map<String, dynamic>? queryParameters,
     T Function(dynamic data)? decoder,
--- test/test_utils/mocks.dart
+++ test/test_utils/mocks.dart
@@ -1,8 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:mockito/annotations.dart';
 import 'package:peibo_flutter_technical_test/chore/network/http/dio_client/dio_client.dart';
+import 'package:peibo_flutter_technical_test/chore/utils/result.dart';
 
-@GenerateMocks([Dio, DioApiClient])
+@GenerateMocks([Dio, DioApiClient,Result])
 // not required for mocks
 // ignore: no-empty-block
 void main() {}
