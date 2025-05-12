500da280e6a8186a6c5ae403992bf96ca5a20f13
Carlos Hernández <minusgat@gmail.com>
2025-05-12 12:46:10 -0600

    simplify PokemonListPage and adjusting item display

--- lib/feature/pokemon/presentation/pokemon_list/pokemon_list_page.dart
+++ lib/feature/pokemon/presentation/pokemon_list/pokemon_list_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:peibo_flutter_technical_test/common/widgets/custom_app_bar.dart';
-import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/pokemon_list/pokemon_item.dart';
+import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/pokemon_item/pokemon_item.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/pokemon_list/pokemon_list_view_provider.dart';
 
 class PokemonListPage extends ConsumerStatefulWidget {
@@ -36,41 +35,23 @@ class _PokemonListPagesState extends ConsumerState<PokemonListPage> {
     final asyncValue = ref.watch(pokemonListProvider);
 
     return Scaffold(
-      appBar: _appBar(context),
       body: asyncValue.when(
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (e, _) => Center(child: Text('Error: $e')),
         data:
             (pokemons) => Padding(
-              padding: const EdgeInsets.only(top: 16.0, left: 16, right: 16),
+              padding: const EdgeInsets.only(top: 16.0, left: 16),
               child: ListView.builder(
                 controller: _controller,
                 itemCount: pokemons.length,
                 physics: const BouncingScrollPhysics(),
                 itemBuilder: (context, index) {
                   final currentItem = pokemons[index];
-                  final isCenter = index % 3 == 0;
 
-                  return PokemonItem(pokemon: currentItem, isCenter: isCenter);
+                  return PokemonItem(pokemon: currentItem, size: 460);
                 },
               ),
             ),
-      ),
-    );
-  }
-
-  CustomAppBar _appBar(BuildContext context) {
-    return CustomAppBar(
-      toolbarHeight: 90,
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Select Your', style: Theme.of(context).textTheme.headlineLarge),
-          Padding(
-            padding: const EdgeInsets.only(left: 64.0),
-            child: Image.asset(width: 156, 'assets/pokemon_logo.png'),
-          ),
-        ],
       ),
     );
   }
