798917f92017864920633c04a4042211d262df7f
Carlos Hernández <minusgat@gmail.com>
2025-05-14 07:24:30 -0600

    implement getPokemon method with error handling and mapping

--- lib/feature/pokemon/data/repository/pokemon_repository_impl.dart
+++ lib/feature/pokemon/data/repository/pokemon_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:peibo_flutter_technical_test/chore/utils/result.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/data/data_source/pokemon_remote_data_source.dart';
+import 'package:peibo_flutter_technical_test/feature/pokemon/data/mappers/pokemon_detail_model_mapper.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/data/mappers/pokemon_model_mapper.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/domain/entities/pokemon.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/domain/repository/pokemon_repository.dart';
@@ -39,5 +40,36 @@ class PokemonRepositoryImpl extends PokemonRepository {
     }
 
     return Result.success(result);
+  }
+
+  @override
+  Future<Result<Pokemon>> getPokemon({required int id}) {
+    return remoteDataSource.getPokemon(id: id).then((r) async {
+      if (r.isFailure) {
+        return Result.failure(
+          r.asFailureOrNull()?.message ?? 'Failed to load pokemon',
+        );
+      }
+
+      final pokemonDetail = r.asSuccessOrNull()?.data;
+      if (pokemonDetail == null) {
+        return Result.failure('Failed to load pokemon');
+      }
+
+      final retrieveSpecie = await remoteDataSource.getPokemonSpecie(id: id);
+      final pokemonSpecie = retrieveSpecie.asSuccessOrNull()?.data;
+
+      final result =
+          pokemonDetail
+              .toDomain(pokemonSpecieModel: pokemonSpecie)
+              .asSuccessOrNull()
+              ?.data;
+
+      if (result == null) {
+        return Result.failure('Failed to map pokemon');
+      }
+
+      return Result.success(result);
+    });
   }
 }
