59671e63c3158c64fdaa4e5750826bfeb5786ad8
Carlos Hernández <minusgat@gmail.com>
2025-05-15 08:46:49 -0600

    update color handling in CustomBadge

--- lib/common/widgets/custom_badge.dart
+++ lib/common/widgets/custom_badge.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:peibo_flutter_technical_test/chore/configuration/app_colors.dart';
 
 class CustomBadge extends StatelessWidget {
-  const CustomBadge({super.key, required this.text});
+  const CustomBadge({super.key, required this.text, this.color});
 
   final String text;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.grey[200],
+        color: color ?? AppColors.whiteBase,
         shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(16),
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 1.0),
