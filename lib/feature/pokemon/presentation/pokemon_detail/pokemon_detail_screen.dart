8929d88f6680a17ca4112b8fbd3063c1524b7355
Carlos Hernández <minusgat@gmail.com>
2025-05-16 02:10:44 -0600

    restructure layout for better organization and readability

--- lib/feature/pokemon/presentation/pokemon_detail/pokemon_detail_screen.dart
+++ lib/feature/pokemon/presentation/pokemon_detail/pokemon_detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:peibo_flutter_technical_test/common/widgets/custom_app_bar.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/domain/entities/pokemon.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/pokemon_detail/pokemon_detail.dart';
+import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/pokemon_detail/widgets/pokemon_detail_circle_background.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/pokemon_detail/widgets/pokemon_detail_surface_color.dart';
 
 class PokemonDetailScreen extends ConsumerWidget {
@@ -17,19 +18,36 @@ class PokemonDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    final toolbarHeight = 90.0;
+
     return Scaffold(
       body: Stack(
         children: [
           PokemonDetailSurfaceColor(pokemon.id),
-          Column(
-            children: [
-              CustomAppBar(
-                toolbarHeight: 90,
-                leading: _leadingButton(context),
-                title: Image.asset('assets/pokemon_logo.png', height: 90),
+          ConstrainedBox(
+            constraints: BoxConstraints(maxHeight: height * 0.8),
+            child: Transform.translate(
+              offset: Offset(0, toolbarHeight),
+              child: Transform.scale(
+                scale: 1.4,
+                child: PokemonDetailCircleBackground(
+                  width: width,
+                  id: pokemon.id,
+                ),
               ),
-              PokemonItemDetail(pokemon: pokemon, size: size),
-            ],
+            ),
+          ),
+          Transform.scale(
+            scale: 1.2,
+            child: PokemonItemDetail(pokemon: pokemon, size: size),
+          ),
+          CustomAppBar(
+            toolbarHeight: toolbarHeight,
+            leading: _leadingButton(context),
+            title: Image.asset('assets/pokemon_logo.png', height: 90),
           ),
         ],
       ),
