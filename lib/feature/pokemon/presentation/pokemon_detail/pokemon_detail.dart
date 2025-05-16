521191510baff3f7ddaecf73ba33669434b0817f
Carlos Hernández <minusgat@gmail.com>
2025-05-16 02:10:33 -0600

    clean up PokemonItemDetail widget and improve name capitalization

--- lib/feature/pokemon/presentation/pokemon_detail/pokemon_detail.dart
+++ lib/feature/pokemon/presentation/pokemon_detail/pokemon_detail.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/domain/entities/pokemon.dart';
+import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/common/utils/pokemon_name_formater.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/common/widgets/pokemon_image.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/pokemon_detail/widgets/pokemon_detail_badge.dart';
-import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/pokemon_detail/widgets/pokemon_detail_circle_background.dart';
 
 class PokemonItemDetail extends StatelessWidget {
   const PokemonItemDetail({
@@ -16,28 +16,30 @@ class PokemonItemDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
       child: Stack(
         alignment: Alignment.center,
         children: [
-          Transform.scale(
-            scale: 1.3,
-            child: PokemonDetailCircleBackground(width: width, id: pokemon.id),
-          ),
           PokemonImage(size: size, imageUrl: pokemon.photoUrl),
           Positioned(
             bottom: 0,
             child: Column(
-              mainAxisSize: MainAxisSize.min,
               children: [
                 Padding(
                   padding: const EdgeInsets.only(top: 12.0),
                   child: PokemonDetailBadge(id: pokemon.id),
                 ),
-                Text(' ${pokemon.name}', style: const TextStyle(fontSize: 64)),
+                Transform.translate(
+                  offset: const Offset(0, -20),
+                  child: Text(
+                    ' ${pokemon.name.capitalize}',
+                    style: const TextStyle(
+                      fontSize: 64,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
