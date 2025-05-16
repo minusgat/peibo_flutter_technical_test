413442db1731559c996e03167cf13eeaae29e602
Carlos Hernández <minusgat@gmail.com>
2025-05-16 02:13:26 -0600

    add PokemonDescription widget and refactor PokemonImage widget for improved functionality

--- lib/feature/pokemon/presentation/common/widgets/pokemon_image.dart
+++ lib/feature/pokemon/presentation/common/widgets/pokemon_image.dart
@@ -1,8 +1,9 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:peibo_flutter_technical_test/common/widgets/custom_hero_image.dart';
+import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/common/widgets/pokeball.dart';
 
-class PokemonImageWidget extends StatelessWidget {
-  const PokemonImageWidget({
+class PokemonImage extends StatelessWidget {
+  const PokemonImage({
     super.key,
     required this.size,
     this.padding = 16,
@@ -11,13 +12,20 @@ class PokemonImageWidget extends StatelessWidget {
 
   final double size;
   final double padding;
-  final String imageUrl;
+  final String? imageUrl;
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: imageUrl,
-      child: CachedNetworkImage(imageUrl: imageUrl, width: size, height: size),
+    return Padding(
+      padding: EdgeInsets.all(padding),
+      child:
+          imageUrl == null
+              ? Pokeball(size: size * .6)
+              : CustomHeroImage(
+                size: size,
+                imageUrl: imageUrl!,
+                placeholder: Pokeball(size: size),
+              ),
     );
   }
 }
