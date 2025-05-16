aef141682b8437435221db83918692cff57b6a93
Carlos Hernández <minusgat@gmail.com>
2025-05-16 02:48:54 -0600

    refactor PokemonItem widget for improved structure and readability

--- lib/feature/pokemon/presentation/pokemon_item/pokemon_item.dart
+++ lib/feature/pokemon/presentation/pokemon_item/pokemon_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:peibo_flutter_technical_test/common/widgets/custom_badge.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/domain/entities/pokemon.dart';
-import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/common/widgets/pokeball.dart';
+import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/common/widgets/pokemon_background.dart';
+import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/common/widgets/pokemon_description.dart';
 import 'package:peibo_flutter_technical_test/feature/pokemon/presentation/common/widgets/pokemon_image.dart';
 
 class PokemonItem extends StatelessWidget {
@@ -12,54 +12,17 @@ class PokemonItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final horizontalOffset = width * .3;
+
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
       child: Stack(
+        alignment: Alignment.center,
         children: [
-          _backgroundDecoration(context),
-          _pokemonBody(),
-          _pokemonDescription(),
-        ],
-      ),
-    );
-  }
-
-  Widget _pokemonBody() {
-    return pokemon.photoUrl == null
-        ? Align(
-          alignment: Alignment.centerRight,
-          child: Pokeball(size: size * .6),
-        )
-        : PokemonImage(size: size, imageUrl: pokemon.photoUrl!);
-  }
-
-  Transform _backgroundDecoration(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final horizontalOffset = width * .3;
-    return Transform.translate(
-      offset: Offset(horizontalOffset, 0),
-      child: Container(
-        width: size,
-        height: size,
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          shape: BoxShape.circle,
-        ),
-      ),
-    );
-  }
-
-  Positioned _pokemonDescription() {
-    return Positioned(
-      bottom: 0,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: CustomBadge(text: "${pokemon.id}"),
-          ),
-          Text(' ${pokemon.name}', style: const TextStyle(fontSize: 36)),
+          PokemonBackground(size: size, offset: horizontalOffset),
+          PokemonImage(size: size, imageUrl: pokemon.photoUrl),
+          Positioned(bottom: 0, child: PokemonDescription(pokemon: pokemon)),
         ],
       ),
     );
